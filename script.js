7d3c7e6d84d0c4216928b6d8467886a9fddf2a36
Sahasi52 <137809801+Sahasi52@users.noreply.github.com>
2025-04-22 19:07:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const wrapper = document.querySelector(".wrapper");
 const btnLogin_popup = document.querySelector(".btnLogin-popup");
-const loginLink = document.querySelector(".loginLink");
-const registerLink = document.querySelector(".registerLink");
 const iconClose = document.querySelector(".iconClose");
+const inputBox = document.querySelector(".inputBox input");
+const loginBtn = document.querySelector(".loginBtn");
+const registerLink = document.querySelector(".registerLink");
+const registerBtn = document.querySelector(".registerBtn");
+const loginLink = document.querySelector(".loginLink");
 btnLogin_popup.addEventListener("click", () => {
   wrapper.classList.remove("active");
   wrapper.classList.add("active-popup");
@@ -10,15 +13,34 @@ btnLogin_popup.addEventListener("click", () => {
   btnLogin_popup.style.border = "2px solid limegreen";
   btnLogin_popup.style.color = "white";
 });
-loginLink.addEventListener("click", () => {
-  wrapper.classList.remove("active");
-});
-registerLink.addEventListener("click", () => {
-  wrapper.classList.add("active");
-});
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
   btnLogin_popup.style.background = "transparent";
   btnLogin_popup.style.border = "2px solid black";
   btnLogin_popup.style.color = "black";
 });
+loginBtn.addEventListener("click", (e) => {
+  if (inputBox.value === "") {
+    e.preventDefault();
+    displayError("Please enter your login details.");
+  } else {
+    wrapper.classList.remove("active-popup");
+  }
+});
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+registerBtn.addEventListener("click", (e) => {
+  if (inputBox.value === "") {
+    e.preventDefault();
+    displayError("Please enter your registration details.");
+  } else {
+    wrapper.classList.remove("active");
+  }
+});
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+function displayError(message) {
+  alert(message);
+}
