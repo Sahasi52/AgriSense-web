ad8d7751f8c6757edf9461614b954fb30465e9b6
Sahasi52 <137809801+Sahasi52@users.noreply.github.com>
2025-04-25 17:09:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-const wrapper = document.querySelector(".wrapper");
 const btnLogin_popup = document.querySelector(".btnLogin-popup");
+const btnCrop_popup = document.querySelector(".btnCrop-popup");
+const btnFertilizer_popup = document.querySelector(".btnFertilizer-popup");
+const wrapper = document.querySelector(".wrapper");
 const iconClose = document.querySelector(".iconClose");
-const inputBox = document.querySelector(".inputBox input");
+const loginEmail = document.querySelector(".loginEmail");
+const loginPassword = document.querySelector(".loginPassword");
+const forgotPassword = document.querySelector(".forgotPassword");
 const loginBtn = document.querySelector(".loginBtn");
 const registerLink = document.querySelector(".registerLink");
+const registerUsername = document.querySelector(".registerUsername");
+const registerEmail = document.querySelector(".registerEmail");
+const registerPassword = document.querySelector(".registerPassword");
 const registerBtn = document.querySelector(".registerBtn");
 const loginLink = document.querySelector(".loginLink");
+const cover = document.querySelector(".cover");
+const iconBack = document.querySelector(".iconBack");
 btnLogin_popup.addEventListener("click", () => {
   wrapper.classList.remove("active");
   wrapper.classList.add("active-popup");
-  btnLogin_popup.style.backgroundColor = "limegreen";
-  btnLogin_popup.style.border = "2px solid limegreen";
-  btnLogin_popup.style.color = "white";
+  btnLogin_popup.classList.add("active");
 });
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
-  btnLogin_popup.style.background = "transparent";
-  btnLogin_popup.style.border = "2px solid black";
-  btnLogin_popup.style.color = "black";
+  btnLogin_popup.classList.remove("active");
 });
-loginBtn.addEventListener("click", (e) => {
-  if (inputBox.value === "") {
-    e.preventDefault();
+forgotPassword.addEventListener("click", () => {
+  displayError("Click here to reset it.");
+});
+loginBtn.addEventListener("click", () => {
+  if (loginEmail.value === "" || loginPassword.value === "") {
     displayError("Please enter your login details.");
   } else {
     wrapper.classList.remove("active-popup");
@@ -30,9 +37,12 @@ loginBtn.addEventListener("click", (e) => {
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
 });
-registerBtn.addEventListener("click", (e) => {
-  if (inputBox.value === "") {
-    e.preventDefault();
+registerBtn.addEventListener("click", () => {
+  if (
+    registerUsername.value === "" ||
+    registerEmail.value === "" ||
+    registerPassword.value === ""
+  ) {
     displayError("Please enter your registration details.");
   } else {
     wrapper.classList.remove("active");
@@ -44,3 +54,14 @@ loginLink.addEventListener("click", () => {
 function displayError(message) {
   alert(message);
 }
+btnCrop_popup.addEventListener("click", () => {
+  cover.classList.remove("active");
+  cover.classList.add("active-popup");
+});
+btnFertilizer_popup.addEventListener("click", () => {
+  cover.classList.add("active");
+  cover.classList.add("active-popup");
+});
+iconBack.addEventListener("click", () => {
+  cover.classList.remove("active-popup");
+});
